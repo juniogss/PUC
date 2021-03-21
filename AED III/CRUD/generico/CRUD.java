16e61cab60ff3eaa39bacf64d072d81dd4828d35
Junio Gabriel <junio.gss@hotmail.com>
2021-03-21 01:12:46 -0300

    CRUD generico

---
+++ AED III/CRUD/generico/CRUD.java
@@ -0,0 +1,215 @@
+import java.io.RandomAccessFile;
+import java.lang.reflect.Constructor;
+
+public class CRUD<T extends Registro> {
+
+    Constructor<T> constructor;
+    protected RandomAccessFile arq;
+
+    CRUD(Constructor<T> constructor, String fileName) throws Exception {
+        this.constructor = constructor;
+        arq = new RandomAccessFile(fileName, "rw");
+        arq.writeInt(0);
+    }
+
+    public void close() throws Exception {
+        arq.close();
+        System.out.println("O CRUD foi encerrado...");
+    }
+
+    public int create(T object) throws Exception {
+        int newID = -1;
+        if (object.getID() == -1) {
+            boolean keepReading = true;
+            short regLength;
+            int lastID;
+            long currentlyPos;
+            byte[] reg;
+            arq.seek(0);
+            lastID = arq.readInt();
+            newID = lastID + 1;
+
+            if (lastID > 0)
+                do {
+                    arq.readByte();
+                    regLength = arq.readShort();
+                    currentlyPos = arq.getFilePointer();
+                    arq.seek(currentlyPos + regLength);
+
+                    if (arq.getFilePointer() >= arq.length())
+                        keepReading = false;
+                } while (keepReading);
+
+            object.setID(newID);
+            reg = object.toByteArray();
+            arq.writeByte(0);
+            arq.writeShort(reg.length);
+            arq.write(reg);
+            arq.seek(0);
+            arq.writeInt(newID);
+        } else
+            System.out.println("CREATE: ID do objeto invalido");
+
+        return newID;
+    }
+
+    public T read(int ID) throws Exception {
+
+        T object = null;
+
+        if (ID > 0) {
+            arq.seek(0);
+            int lastID = arq.readInt();
+
+            if (lastID > 0) {
+                long currentlyPos;
+                boolean keepReading = true;
+                boolean founded = false;
+                byte lapide;
+
+                short regLength;
+                int currentlyID;
+
+                do {
+                    lapide = arq.readByte();
+                    regLength = arq.readShort();
+                    currentlyPos = arq.getFilePointer();
+                    currentlyID = arq.readInt();
+                    arq.seek(currentlyPos + regLength);
+
+                    if (arq.getFilePointer() >= arq.length())
+                        keepReading = false;
+                    if (lapide == 0 && currentlyID == ID) {
+                        keepReading = false;
+                        founded = true;
+                        object = this.constructor.newInstance();
+
+                        byte[] ba = new byte[regLength];
+                        arq.seek(currentlyPos);
+                        arq.read(ba);
+                        object.fromByteArray(ba);
+                    }
+                } while (keepReading);
+                if (!founded)
+                    System.out.println("READ: O registro com o ID buscado não foi encontrado.");
+            } else
+                System.out.println("READ: Nenhum registro foi inserido no arquivo.");
+        } else
+            System.out.println("READ: ID fornecido invalido.");
+
+        return object;
+    }
+
+    public boolean delete(int ID) throws Exception {
+
+        boolean operationStatus = false;
+
+        if (ID > 0) {
+            arq.seek(0);
+            int lastID = arq.readInt();
+
+            if (lastID > 0) {
+                long currentlyPos;
+                long lapidePos;
+                boolean keepReading = true;
+                boolean founded = false;
+                byte lapide;
+                short regLength;
+                int currentlyID;
+
+                do {
+                    lapidePos = arq.getFilePointer();
+                    lapide = arq.readByte();
+                    regLength = arq.readShort();
+                    currentlyPos = arq.getFilePointer();
+                    currentlyID = arq.readInt();
+                    arq.seek(currentlyPos + regLength);
+
+                    if (arq.getFilePointer() >= arq.length())
+                        keepReading = false;
+                    if (lapide == 0 && ID == currentlyID) {
+                        keepReading = false;
+                        founded = true;
+                        arq.seek(lapidePos);
+                        arq.writeByte(1);
+                        operationStatus = true;
+                    }
+                } while (keepReading);
+
+                if (!founded)
+                    System.out.println("DELETE: O registro com o ID buscado não foi encontrado.");
+            } else
+                System.out.println(
+                        "DELETE: A exclusão do registro não foi concluida. Nenhum registro foi inserido no arquivo.");
+        } else
+            System.out.println("DELETE: A exclusão do registro não foi concluida. O ID fornecido é invalido.");
+
+        return operationStatus;
+    }
+
+    public boolean update(T object) throws Exception {
+
+        boolean operationStatus = false;
+        int ID = object.getID();
+
+        if (ID > 0) {
+            arq.seek(0);
+            int lastID = arq.readInt();
+
+            if (lastID > 0) {
+                long lapidePos;
+                long currentlyPos;
+                boolean bigger = false;
+                boolean keepReading = true;
+                boolean founded = false;
+                byte lapide;
+                short regLength;
+                int currentlyID;
+                byte[] newBa = object.toByteArray();
+
+                do {
+                    lapidePos = arq.getFilePointer();
+                    lapide = arq.readByte();
+                    regLength = arq.readShort();
+                    currentlyPos = arq.getFilePointer();
+                    currentlyID = arq.readInt();
+                    arq.seek(currentlyPos + regLength);
+
+                    if (arq.getFilePointer() >= arq.length())
+                        keepReading = false;
+                    if (lapide == 0 && ID == currentlyID) {
+                        operationStatus = true;
+                        founded = true;
+                        byte[] oldBa = new byte[regLength];
+                        arq.seek(currentlyPos);
+                        arq.read(oldBa);
+                        if (newBa.length <= oldBa.length) {
+                            keepReading = false;
+                            arq.seek(currentlyPos);
+                            arq.write(newBa);
+                        } else {
+                            bigger = true;
+                            arq.seek(lapidePos);
+                            arq.writeByte(1);
+
+                            arq.seek(currentlyPos + regLength);
+
+                        }
+                    }
+                } while (keepReading);
+
+                if (bigger) {
+                    arq.writeByte(0);
+                    arq.writeShort(newBa.length);
+                    arq.write(newBa);
+                }
+                if (!founded)
+                    System.out.println("UPDATE: O registro com o ID buscado não foi encontrado.");
+            } else
+                System.out.println("UPDATE: Nenhum registro foi inserido no arquivo.");
+        } else
+            System.out.println("UPDATE: A atualizaçãp do registro não foi concluida. O ID fornecido é invalido.");
+
+        return operationStatus;
+    }
+}
---
+++ AED III/CRUD/generico/Main.java
@@ -0,0 +1,50 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EnergyDrink drink1 = new EnergyDrink("Red Bull", "Melancia", 11.50F);
+        EnergyDrink drink2 = new EnergyDrink("Red Bull", "Açaí", 11.50F);
+        EnergyDrink drink3 = new EnergyDrink("Loud", "Original", 8.00F);
+
+        try {
+            new File("energeticos.db").delete();
+            CRUD<EnergyDrink> arq = new CRUD<>(EnergyDrink.class.getConstructor(), "energeticos.db");
+
+            // Insere os 3 energeticos
+            System.out.println("\nCREATE");
+            int id1 = arq.create(drink1);
+            int id2 = arq.create(drink2);
+            int id3 = arq.create(drink3);
+
+            // Busca 3 energeticos
+            System.out.println("\nREAD");
+            System.out.println(arq.read(id1));
+            System.out.println(arq.read(id2));
+            System.out.println(arq.read(id3));
+
+            System.out.println("\nUPDATE");
+            drink1.setFlavor("Fruras tropicais");
+            drink1.setPrice(9.50F);
+            arq.update(drink1);
+            System.out.println(arq.read(id1));
+
+            drink2.setFlavor("Sem açúcar");
+            drink2.setPrice(9.50F);
+            arq.update(drink2);
+            System.out.println(arq.read(id2));
+
+            System.out.println("\nDELETE");
+            System.out.println(arq.read(id1));
+            arq.delete(id1);
+            EnergyDrink aux = arq.read(id1);
+            System.out.println(aux == null ? "Energético excluído\n" : aux);
+
+            arq.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
